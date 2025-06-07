7294c8eccb6862b0cea0dcaf185d8af3b31e581f
unknown <sreeprathikshaa@gmail.com>
2025-06-07 12:03:00 +0530

    updated js file

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple contact form validation alert
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thanks for contacting me! I will get back to you soon.");
+});
